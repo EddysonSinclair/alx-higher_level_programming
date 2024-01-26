cd69238bbdff1eceff63e968b2ad66bf6223f9b5
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-01-26 09:42:12 +0000

    print number

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 number = 98
-# store the variable battery street in a variable and print using F string
-street = 'Battery street'
-print(f"{number} {street} \n")
+print(f"{number} Battery street")
