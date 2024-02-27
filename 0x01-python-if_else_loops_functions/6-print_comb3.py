800479d9447f3247d6ec5a16c198715701495760
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-02-27 20:46:07 +0000

    combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(9):
     for j in range(i + 1, 10):
         if i < 8:
-            print("{}{}".format(i % 10, j % 10),end=", ")
+            print("{}{}".format(i % 10, j % 10), end=", ")
         else:
-            print("{}{}".format(i % 10, j % 10),end="\n")
+            print("{}{}".format(i % 10, j % 10), end="\n")
