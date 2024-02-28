00f2e237e252df38724aa55080ba0aae04a67b5c
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-02-28 14:18:28 +0000

    to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def uppercase(c):
+def uppercase(str):
     result = ''
-    for i in c:
+    for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             result += chr(ord(i) - 32)
         else:
