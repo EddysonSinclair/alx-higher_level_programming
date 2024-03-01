c21eb8ba95eb151df38fae6674eed3ccb747206d
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-01 10:46:52 +0000

    tebalpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 count = 1;
-for i in range(122, 97, -1):
+for i in range(122, 96, -1):
     if count % 2 == 0:
         print("{}".format(chr(i - 32)), end="")
         count += 1
