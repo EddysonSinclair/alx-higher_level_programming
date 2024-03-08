1ba046520aa7cd8e24383f860d4d2dc303955870
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-08 11:10:50 +0000

    modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                 print('{} - {} = {}'.format(a, b, sub(a, b)))
             elif argv[i * 2 + 2] == '*':
                 print('{} * {} = {}'.format(a, b, mul(a, b)))
-            elif argv[i * 2 + 2] == '+':
+            elif argv[i * 2 + 2] == '/:w':
                 print('{} / {} = {}'.format(a, b, div(a, b)))
             else:
                 print("Unknown operator. Available operators: +, -, * and /")
