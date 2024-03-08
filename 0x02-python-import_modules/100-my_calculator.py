8748134d478155954969fb8942090c66a7940acb
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-08 11:18:53 +0000

    modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,17 +8,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        for i in range(0, num, 3):
-            a = int(argv[i * 2 + 1 ])
-            b = int(argv[i * 2 + 3])
-            if argv[i * 2 + 2] == '+':
-                print('{} + {} = {}'.format(a, b, add(a, b)))
-            elif argv[i * 2 + 2] == '-':
-                print('{} - {} = {}'.format(a, b, sub(a, b)))
-            elif argv[i * 2 + 2] == '*':
-                print('{} * {} = {}'.format(a, b, mul(a, b)))
-            elif argv[i * 2 + 2] == '/':
-                print('{} / {} = {}'.format(a, b, div(a, b)))
-            else:
-                print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
+        a = int(argv[1])
+        op = argv[2]
+        b = int(argv[3])
+        if op == '+':
+            print('{} + {} = {}'.format(a, b, add(a, b)))
+        elif op == '-':
+            print('{} - {} = {}'.format(a, b, sub(a, b)))
+        elif op == '*':
+            print('{} * {} = {}'.format(a, b, mul(a, b)))
+        elif op == '/':
+            print('{} / {} = {}'.format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
