4b88f9e7b7ef4f8e76a37a1dee272963cebca5ca
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-11 15:37:52 +0000

    modules

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    num = len(my_list)
+    a = -1
+    for i in range (num):
+        print("{}".format(my_list[a]))
+        a += -1
