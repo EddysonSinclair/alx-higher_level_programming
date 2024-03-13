ceae9f68f4609628583cee535a763fd9cf681bff
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-13 11:44:50 +0000

    modules

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     if sentence == None:
-        return (length, None)
+        first = None
+        return (length, First)
     else:
         return (length, first)
