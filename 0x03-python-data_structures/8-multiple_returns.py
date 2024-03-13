5310f3e9ff3ceffaacbdfdf4ae51c3631eb80e3d
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-13 12:31:14 +0000

    modules

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,2 @@
 def multiple_returns(sentence):
-    if sentence is None:
-        first = None
-        length = 0
-        newtup = (length, first)
-    else:
-        first = sentence[0]
-        newtup = (len(sentence), first)
-    return (newtup)
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
