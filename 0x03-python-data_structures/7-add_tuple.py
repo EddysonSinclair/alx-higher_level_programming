0de702c015b36a1d0650fbe6b0b5a9b7c1310916
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-13 11:27:40 +0000

    modules

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+    a, b = tuple_a[:2]
+    c, d = tuple_b[:2]
+
+    sum_1 = a + c
+    sum_2 = b + d
+    newtup = (sum_1, sum_2)
+    return (newtup)
+
+
+
