76425d12d1c42710353a6ff19a8e408d4a3500c0
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-14 00:22:21 +0000

    modules

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    new = [True if num % 2 == 0 else False for num in my_list]
+    return (new)
