f8f0f91842953f9dcfb15f239c2d0b42d5ac0972
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-18 23:21:57 +0000

    modules

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * is_palindrome - recurs palindrome
+ * @head: head list
+ * Return: integer
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (auxilary_palindrome, *head);
+}
+/**
+ * auauxilary_palindrome - function  to know if a number is a palindrome
+ * @head: head list
+ * @end: end list
+ * Return: integer
+ */
+int auauxilary_palindrome(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+		return (1);
+	if (auauxilary_palindrome(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
