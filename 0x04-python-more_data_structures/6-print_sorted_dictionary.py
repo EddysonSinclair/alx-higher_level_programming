b6b6cbff7d20ea215a3c0fef9bbdef14466d524c
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-21 15:44:22 +0000

    more data struct

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for a, b in sorted(a_dictionary.items()):
-        print(f"{a}: {b}")
+    """Print a dictionary by ordered keys."""
+    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
