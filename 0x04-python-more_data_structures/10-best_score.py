ea1b6f9c9ce0b7ba0cd21d0fe3b45edeb0242087
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-23 00:42:10 +0000

    more structs

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_value = 0
+    if a_dictionary == None or not a_dictionary:
+        return (None)
+    for k, v in a_dictionary.items():
+        if v > max_value:
+            max_value = v
+            best_key = k
+    return (best_key)
