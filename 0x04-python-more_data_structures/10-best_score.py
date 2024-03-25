77925ca6c944645490b013624b06c1905fe83025
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-25 11:38:34 +0000

    more structs

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     best_key = None
     max_value = 0
-    if a_dictionary == None or not a_dictionary:
+    if a_dictionary is None or not a_dictionary:
         return (None)
     for k, v in a_dictionary.items():
         if v > max_value:
