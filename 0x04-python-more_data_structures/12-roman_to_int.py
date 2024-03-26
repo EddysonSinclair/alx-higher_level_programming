8734b2df934484015adb3cb2fbd5b6051f056af9
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-26 22:52:37 +0000

    more data struct

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,8 +13,9 @@ def roman_to_int(roman_string):
             'M': 1000
             }
     for i in range(len(roman_string)):
-        if (roman_dict.get(roman_string[i], 0) == 0):
+        if (roman_dict.get(roman_string[i]) == 0):
             return (0)
+
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
             total -= roman_dict[roman_string[i]]
