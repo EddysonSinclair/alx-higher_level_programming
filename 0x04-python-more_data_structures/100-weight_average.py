96ff2dcf77017c4f4d0fcc5f7b52a547975ad60c
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-27 15:56:51 +0000

    weight average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    pdt_score = 0
+    weight = 0
+
+    for element in my_list:
+        pdt_score += element[0] * element[1]
+        weight += element[1]
+    return (pdt_score/weight)
