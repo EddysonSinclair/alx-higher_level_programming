464c0978feac0e25a9f9a531d51f4742276708d8
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-03-31 14:29:35 +0000

    safe print

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,8 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return (None)
     finally:
-        if 'result' in locals():
-            print("Inside result: {}".format(result))
-        else:
-            print("Inside result: {}".format(None))
+        print("Inside result: {}".format(result))
         return(result)
