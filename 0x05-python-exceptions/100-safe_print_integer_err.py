aba527d678bb711a5872334dc001b42dd9b19bb9
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-04-04 02:02:47 +0000

    exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,10 +7,11 @@ The integer should be printed followed by a new line
 Returns True if value has been correctly printed
 """
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
-    except ValueError as e:
+    except (TypeError, ValueError) as e:
         sys.stderr.write("Exception: {}\n".format(e))
         return (False)
