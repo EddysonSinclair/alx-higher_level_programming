0eb1048a1361a361f75670d9dd0d58cfac92db55
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-04-04 01:56:10 +0000

    exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import sys
-
+"""
+Write a function that prints an integer.
+value can be any type (integer, string, etc.)
+The integer should be printed followed by a new line
+Returns True if value has been correctly printed
+"""
 
 def safe_print_integer_err(value):
     try:
