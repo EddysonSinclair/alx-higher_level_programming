2ed8f36358e48cb1d51cd3dbb9b9b702c58b17e9
Eddysonsinclair <eddysonsinclair@gmail.com>
2024-04-10 15:29:00 +0000

    OOPS

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class Square"""
+
+
+class Square:
+    """Defining a Square"""
+    def __init__(self, __size=0):
+        self.__size = __size
+        if type(__size) is not int:
+            raise TypeError("size must be an integer")
+        if (__size < 0):
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return (pow(self.__size, 2))
