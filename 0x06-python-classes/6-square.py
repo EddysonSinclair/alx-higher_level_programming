df57246c10fcd252671663f48bd6a69587c71116
Eddyson Sinclair <Eddysonsinclair@gmail.com>
2024-04-12 23:31:36 +0000

    OOPS

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,5 +44,5 @@ class Square:
         for _ in range(self.position[1]):
             print()
         for _ in range(self.size):
-            print("_" * self.position[0], end="")
+            print(" " * self.position[0], end="")
             print("#" * self.size)
