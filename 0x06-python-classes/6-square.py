742e08aa8136322b27f7b0c551b7e63e8e9191f3
Eddyson Sinclair <Eddysonsinclair@gmail.com>
2024-04-13 00:43:54 +0000

    OOPS

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,24 +4,10 @@
 
 class Square:
     """Defining a Square"""
+
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
-
-    """ Getter function for position"""
-    @property
-    def position(self):
-        return (self.__position)
-
-    """setter function for position"""
-    @position.setter
-    def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     """ Getter function """
     @property
@@ -36,6 +22,20 @@ class Square:
         if (value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    """ Getter function for position"""
+    @property
+    def position(self):
+        return (self.__position)
+    """setter function for position"""
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         return (self.__size * self.__size)
