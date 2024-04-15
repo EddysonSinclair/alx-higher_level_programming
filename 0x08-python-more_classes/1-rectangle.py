23e3dbfe6e27bb4e25f4a92a9c13818e7d612da5
Eddyson Sinclair <Eddysonsinclair@gmail.com>
2024-04-15 11:11:17 +0000

    MORE OOPS

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,22 +8,7 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-    """ this is the property getter for width"""
-    @property
-    def width(self):
-        return(self.__width)
-
-    """ this is a setter for width"""
-    @width.setter
-    def width(self, value):
-        if (not isinstance(value, int)):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-
-    """This is the property getter for height"""
+    """ this is the property getter for height"""
     @property
     def height(self):
         return(self.__height)
@@ -31,9 +16,24 @@ class Rectangle:
     """ this is a setter for height"""
     @height.setter
     def height(self, value):
-        if (not isinstance(value, int)):
+        if (not isinstance(value, int) and value is not None):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    """This is the property getter for width"""
+    @property
+    def width(self):
+        return(self.__width)
+
+    """ this is a setter for width"""
+    @width.setter
+    def width(self, value):
+        if (not isinstance(value, int) and value is not None):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
