1e64f977116bbc944741f1f28280f55713de09ff
Eddyson Sinclair <Eddysonsinclair@gmail.com>
2024-04-18 12:11:28 +0000

    MORE OOPS

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     """ This defines a rectangle"""
 
     def __init__(self, width=0, height=0):
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
         self.width = width
         self.height = height
@@ -69,5 +69,5 @@ class Rectangle:
         return (f"Rectangle({self.width}, {self.height})")
 
     def __del__(self):
-        Rectangle.number_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
