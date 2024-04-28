934ef5522655bb2dd775445010adf432e6004255
Eddyson Sinclair <Eddysonsinclair@gmail.com>
2024-04-28 17:43:39 +0000

    test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,10 +2,10 @@
 
 # this is a function to add two integers
 
+
 def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
     elif not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
     return (int(a) + int(b))
-
