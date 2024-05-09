bd51aa8aec74637b500f62514a5a3a73c4c8facd
Eddyson Sinclair <Eddysonsinclair@gmail.com>
2024-05-09 23:28:15 +0000

    test driven development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This is a function that prints two arguments {first_name} and {last_name}
+ARG1: This represents the first_name.
+ARG2: This represents the second parameter '''last_name'''
+"""
 
 
 def say_my_name(first_name, last_name=""):
@@ -6,4 +11,10 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print(f"My name is {first_name} {last_name}")
+    elif not last_name:
+        print("My name is {}".format(first_name))
+    elif say_my_name is None:
+        raise TypeError("say_my_name() missing 1 required positional"
+                        "argument: 'first_name'")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
