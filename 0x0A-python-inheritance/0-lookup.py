cd66194ee0cdd0e4e0740d32c3a559d94594aeaf
Eddyson Sinclair <Eddysonsinclair@gmail.com>
2024-05-09 19:36:28 +0000

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """
-This is a function that returns the
-list of available attributes and methods of an object
+Return a list of available attributes and methods of an object.
+
+Args:
+    obj: Any Python object.
+    
+Returns:
+    list: A list containing the names of attributes and methods of the object.
 """
 
 
 def lookup(obj):
+    if obj is None:
+        return None
+    if not hasattr(obj, '__dir__'):
+        return None
     return (dir(obj))
